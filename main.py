15e5c610865b17b6d4c7d17dc5d369af01be8a90
wallacce135 <wallacce135@gmail.com>
2021-10-27 12:16:54 +0300

    v1.1 Output in Excel file - Done

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import json
 import os
 import time
+import pandas as pd
+
 
 
 with open('jobsList.json', 'r', encoding='utf-8') as file:
@@ -23,19 +25,34 @@ def getData(vacancyName):
     # Запись полученного с API резльтата
     with open('result.json', 'w', encoding='utf-8') as file:
         file.write(data)
+        file.close()
 
     # Чтение названия из вакансий по профессии
 
+    time.sleep(2)
+    dataArr = {'Название': [], 'Зарплата(От)': [], 'Зарплата(До)': [], 'График': [], 'Ссылка': []}
     with open('result.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
-        for i in range(0,100000000):
-            try:
-                print(data['items'][i]['name'])
-            except IndexError:
-                break
+        for i in range(0, 5):
+
+            # print(i)
+            name = data['items'][i]['name']
+            salaryFrom = data['items'][i]['salary']['from']
+            salaryTo = data['items'][i]['salary']['to']
+            schedule = data['items'][i]['schedule']['name']
+            url = data['items'][i]['alternate_url']
+
+            dataArr['Название'].append(name)
+            dataArr['Зарплата(От)'].append(salaryFrom)
+            dataArr['Зарплата(До)'].append(salaryTo)
+            dataArr['График'].append(schedule)
+            dataArr['Ссылка'].append(url)
+
+    return dataArr
 
 
 # Открытие файла с профессиями для чтения
+
 with open('jobsList.json', 'r', encoding='utf-8') as file:
     values = json.load(file)
     jobList = values['jobList']
@@ -43,19 +60,22 @@ with open('jobsList.json', 'r', encoding='utf-8') as file:
 
 # Вызов функции для каждой профессии
 
+fullData = {'Название': [], 'Зарплата(От)': [], 'Зарплата(До)': [], 'График': [], 'Ссылка': []}
+
 for vacancy in jobList:
-    getData(vacancy)
+    data = getData(vacancy)
+    fullData['Название'] = fullData['Название'] + [vacancy]
+    fullData['Зарплата(От)'] = fullData['Зарплата(От)'] + [""]
+    fullData['Зарплата(До)'] = fullData['Зарплата(До)'] + [""]
+    fullData['График'] = fullData['График'] + [""]
+    fullData['Ссылка'] = fullData['Ссылка'] + [""]
 
 
+    fullData['Название'] = fullData['Название'] + data['Название']
+    fullData['Зарплата(От)'] = fullData['Зарплата(От)'] + data['Зарплата(От)']
+    fullData['Зарплата(До)'] = fullData['Зарплата(До)'] + data['Зарплата(До)']
+    fullData['График'] = fullData['График'] + data['График']
+    fullData['Ссылка'] = fullData['Ссылка'] + data['Ссылка']
 
-
-
-
-# with open('result.json', 'r', encoding='utf-8') as file:
-#     data = json.load(file)
-#     for i in range(0, 100000):
-#         try:
-#             name = data['items'][i]['name']
-#             print(name)
-#         except IndexError:
-#             break
+df = pd.DataFrame(fullData)
+df.to_excel('salary.xlsx', index=False)
